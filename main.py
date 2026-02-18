2ccd419ba67517e4299e6b90f05fb71bf2d69882
jakseluz <labuzjak@gmail.com>
2026-02-18 16:56:14 +0100

    Add basic translation and numbering

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import zipfile
 import xml.etree.ElementTree as ET
 import time
+from word2word import Word2word
 
 start_time = time.time()
 
 text_words = []  # all words from all documents, to be used for NLP analysis in the occurence order
 found_words = dict()  # dictionary to store the count of each word, to be used for statistical analysis and ranking
+w2w = Word2word("el", "en")
+
+
+def translate(word):
+    try:
+        return w2w(word)
+    except Exception as e:
+        # print(f"Error translating word '{word}': {e}")
+        return None
+
 
 # Read the XML files from the zip archive and extract the words and count their occurrences
 with zipfile.ZipFile("./Diorisis.zip") as z:
@@ -25,7 +36,7 @@ with zipfile.ZipFile("./Diorisis.zip") as z:
                             if entry in found_words:
                                 found_words[entry][1] += 1
                             else:
-                                found_words[entry] = [POS, 1]
+                                found_words[entry] = [POS, 1, translate(entry)]
                             text_words.append((entry, POS))
                     elem.clear()
 
@@ -38,18 +49,19 @@ part_time = time.time()
 print(f"Time to sort words by count: {part_time - start_time:.2f} seconds")
 # Create a list of tuples with word, count, rank and product of Zipf's law
 words_ranking = [
-    (word, count, rank, count * rank, POS) for rank, (word, (POS, count)) in enumerate(sorted_by_count, start=1)
+    (word, count, rank, count * rank, POS, translation)
+    for rank, (word, (POS, count, translation)) in enumerate(sorted_by_count, start=1)
 ]
 part_time = time.time()
 print(f"Time to create words ranking: {part_time - start_time:.2f} seconds")
 
 # Print the top 50 words with their count, rank and product of Zipf's law
 print("\n\nSorted by count:\n")
-i = 0
-for word, count, rank, product, POS in words_ranking:
-    if i >= 50:
+i = 1
+for word, count, rank, product, POS, translation in words_ranking:
+    if i > 50:
         break
-    print(f"{word}: {count} (rank: {rank}, product: {product}, POS: {POS})")
+    print(f"{i}. {word}: {count} (rank: {rank}, product: {product}, POS: {POS}, translation: {translation})")
     i += 1
 part_time = time.time()
 print(f"Time to print top 50 words: {part_time - start_time:.2f} seconds")
@@ -60,9 +72,9 @@ import matplotlib.pyplot as plt
 # create a graph with edges between neighbouring words
 first_2000_words = sorted_by_count[:2000]
 part_time = time.time()
-print(f"Time to get first 2000 words: {part_time - start_time:.2f} seconds")
+print(f"\nTime to get first 2000 words: {part_time - start_time:.2f} seconds")
 G = nx.Graph()
-G.add_nodes_from([(word, POS) for word, (POS, _) in first_2000_words])
+G.add_nodes_from([(word, POS) for word, (POS, _, _) in first_2000_words])
 part_time = time.time()
 print(f"Time to add nodes to graph: {part_time - start_time:.2f} seconds")
 for i in range(len(text_words) - 1):
@@ -98,21 +110,21 @@ print(f"Time to add edges to graph: {part_time - start_time:.2f} seconds")
 
 top_words_by_connections = sorted(G.degree(weight="weight"), key=lambda x: x[1], reverse=True)
 print("\n\nTop 200 words by number of connections:\n")
-i = 0
+i = 1
 for (word, POS), degree in top_words_by_connections:
-    if i >= 200:
+    if i > 200:
         break
-    print(f"{word} (POS: {POS}): {degree} connections")
+    print(f"{i}. {word} (POS: {POS}, translation: {translate(word)}): {degree} connections")
     i += 1
 
 print("\n\nTop 50 nouns by number of connections:\n")
-i = 0
-while i < 50 and top_words_by_connections:
+i = 1
+while i <= 50 and top_words_by_connections:
     (word, POS), degree = top_words_by_connections.pop(0)
     if POS == "noun":
-        print(f"{word} (POS: {POS}): {degree} connections")
+        print(f"{i}. {word} (POS: {POS}, translation: {translate(word)}): {degree} connections")
         i += 1
 
 
 end_time = time.time()
-print(f"Execution time: {end_time - start_time:.2f} seconds")
+print(f"\nExecution time: {end_time - start_time:.2f} seconds")
