0fb3e6e4a53eb2ee12bbefe5ac586c2920fb3f6c
jakseluz <labuzjak@gmail.com>
2026-02-18 16:18:29 +0100

    Add listing words with mosts connections and top nouns, as well as 'graph code'

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import zipfile
 import xml.etree.ElementTree as ET
+import time
 
-found_words = dict()
+start_time = time.time()
 
+text_words = []  # all words from all documents, to be used for NLP analysis in the occurence order
+found_words = dict()  # dictionary to store the count of each word, to be used for statistical analysis and ranking
+
+# Read the XML files from the zip archive and extract the words and count their occurrences
 with zipfile.ZipFile("./Diorisis.zip") as z:
     for name in z.namelist():
         if not name.endswith(".xml"):
@@ -15,26 +20,99 @@ with zipfile.ZipFile("./Diorisis.zip") as z:
                     lemma_elem = elem.find("lemma")
                     if lemma_elem is not None:
                         entry = lemma_elem.get("entry")
+                        POS = lemma_elem.get("POS")
                         if entry is not None:
                             if entry in found_words:
-                                found_words[entry] += 1
+                                found_words[entry][1] += 1
                             else:
-                                found_words[entry] = 1
+                                found_words[entry] = [POS, 1]
+                            text_words.append((entry, POS))
                     elem.clear()
 
-i = 0
-for word, count in found_words.items():
-    if i >= 50:
-        break
-    print(f"{word}: {count}")
-    i += 1
+part_time = time.time()
+print(f"Time to read and process XML files: {part_time - start_time:.2f} seconds")
 
-sorted_by_count = sorted(found_words.items(), key=lambda kv: kv[1], reverse=True)
-words_ranking = [(word, count, rank, count * rank) for rank, (word, count) in enumerate(sorted_by_count, start=1)]
+# Sort the words by their count
+sorted_by_count = sorted(found_words.items(), key=lambda kv: kv[1][1], reverse=True)
+part_time = time.time()
+print(f"Time to sort words by count: {part_time - start_time:.2f} seconds")
+# Create a list of tuples with word, count, rank and product of Zipf's law
+words_ranking = [
+    (word, count, rank, count * rank, POS) for rank, (word, (POS, count)) in enumerate(sorted_by_count, start=1)
+]
+part_time = time.time()
+print(f"Time to create words ranking: {part_time - start_time:.2f} seconds")
+
+# Print the top 50 words with their count, rank and product of Zipf's law
 print("\n\nSorted by count:\n")
 i = 0
-for word, count, rank, product in words_ranking:
+for word, count, rank, product, POS in words_ranking:
     if i >= 50:
         break
-    print(f"{word}: {count} (rank: {rank}, product: {product})")
+    print(f"{word}: {count} (rank: {rank}, product: {product}, POS: {POS})")
     i += 1
+part_time = time.time()
+print(f"Time to print top 50 words: {part_time - start_time:.2f} seconds")
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# create a graph with edges between neighbouring words
+first_2000_words = sorted_by_count[:2000]
+part_time = time.time()
+print(f"Time to get first 2000 words: {part_time - start_time:.2f} seconds")
+G = nx.Graph()
+G.add_nodes_from([(word, POS) for word, (POS, _) in first_2000_words])
+part_time = time.time()
+print(f"Time to add nodes to graph: {part_time - start_time:.2f} seconds")
+for i in range(len(text_words) - 1):
+    word1 = text_words[i]
+    word2 = text_words[i + 1]
+    if word1 in G and word2 in G:
+        if G.has_edge(word1, word2):
+            G[word1][word2]["weight"] += 1
+        else:
+            G.add_edge(word1, word2, weight=1)
+    else:
+        continue
+part_time = time.time()
+print(f"Time to add edges to graph: {part_time - start_time:.2f} seconds")
+
+# position = nx.circular_layout(G)
+
+# draw_start = time.time()
+# print("Drawing graph...")
+# # plt.figure(figsize=(16, 16))
+# # nx.draw_networkx_nodes(G, position, node_size=10, node_color="lightblue")
+# part_time = time.time()
+# print(f"Time to draw nodes: {part_time - draw_start:.2f} seconds")
+# # nx.draw_networkx_edges(G, position, width=0.5, alpha=0.5)
+# part_time = time.time()
+# print(f"Time to draw edges: {part_time - draw_start:.2f} seconds")
+# edge_labels = nx.get_edge_attributes(G, "weight")
+# # nx.draw_networkx_edge_labels(G, position, edge_labels=edge_labels, font_size=6)
+# # part_time = time.time()
+# # print(f"Time to draw edge labels: {part_time - draw_start:.2f} seconds")
+# # plt.axis("off")
+# # plt.show()
+
+top_words_by_connections = sorted(G.degree(weight="weight"), key=lambda x: x[1], reverse=True)
+print("\n\nTop 200 words by number of connections:\n")
+i = 0
+for (word, POS), degree in top_words_by_connections:
+    if i >= 200:
+        break
+    print(f"{word} (POS: {POS}): {degree} connections")
+    i += 1
+
+print("\n\nTop 50 nouns by number of connections:\n")
+i = 0
+while i < 50 and top_words_by_connections:
+    (word, POS), degree = top_words_by_connections.pop(0)
+    if POS == "noun":
+        print(f"{word} (POS: {POS}): {degree} connections")
+        i += 1
+
+
+end_time = time.time()
+print(f"Execution time: {end_time - start_time:.2f} seconds")
